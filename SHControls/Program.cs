49473cda91a7935488debb6421fbbe6b5fd8ca55
mountrivers <36880919+mountrivers@users.noreply.github.com>
2021-06-07 21:43:55 +0900

    첫 커밋

--- SHControls/Program.cs
+++ SHControls/Program.cs
@@ -16,7 +16,7 @@ namespace SHControls
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Example1());
         }
     }
 }
